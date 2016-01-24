39245437ed8bd49b6bb6513b011316f0e7990b10
gtg092x <mdrake@mediadrake.com>
2016-01-24 15:27:29 -0800

    webpack pain

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -19,5 +19,23 @@ module.exports = _.defaults({
             }
         }),
         new ExtractTextPlugin('main.css', { allChunks: true }),
-    ]},
+    ],
+        module: {
+            loaders: [
+                { test: /\.jsx?$/,
+                    loader: 'babel',
+                    include: path.join(__dirname, 'app') },
+                { test: /\.jsx?$/,
+                    loader: 'babel',
+                    include: path.join(__dirname, 'app') },
+                { test: /\.json?$/, exclude: /(node_modules|bower_components)/, loader: 'json' }, // JSON FILES
+                {
+                    test: /(\.scss|\.css)$/,
+                    loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
+                },
+                { test: /\.(jpe?g|png|gif|svg)$/i, exclude: /(node_modules|bower_components)/, loader: 'url?limit=1000&name=images/[hash].[ext]' } // IMAGES
+            ]
+        }
+    },
+
     configBase)
