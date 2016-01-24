cf0750e37bc95fbc7f2b475d200f53787d4d9ed8
gtg092x <mdrake@mediadrake.com>
2016-01-24 15:30:14 -0800

    webpack pain

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,15 +1,17 @@
 var path = require('path');
 var webpack = require('webpack');
 var outDir = 'public';
-var configBase = require('./webpack.config.base');
-var _ = require('lodash');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = _.defaults({
+module.exports = {
     devtool: 'source-map',
     entry: [
         './app/entry'
     ],
+    output: {
+        path: path.join(__dirname, outDir),
+        filename: 'bundle.js',
+        publicPath: '/' + outDir + '/'
+    },
     plugins: [
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin({
@@ -17,25 +19,22 @@ module.exports = _.defaults({
             compress: {
                 warnings: false
             }
-        }),
-        new ExtractTextPlugin('main.css', { allChunks: true }),
+        })
     ],
-        module: {
-            loaders: [
-                { test: /\.jsx?$/,
-                    loader: 'babel',
-                    include: path.join(__dirname, 'app') },
-                { test: /\.jsx?$/,
-                    loader: 'babel',
-                    include: path.join(__dirname, 'app') },
-                { test: /\.json?$/, exclude: /(node_modules|bower_components)/, loader: 'json' }, // JSON FILES
-                {
-                    test: /(\.scss|\.css)$/,
-                    loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
-                },
-                { test: /\.(jpe?g|png|gif|svg)$/i, exclude: /(node_modules|bower_components)/, loader: 'url?limit=1000&name=images/[hash].[ext]' } // IMAGES
-            ]
-        }
+    resolve: {
+        extensions: ['', '.js', '.jsx']
     },
-
-    configBase)
+    module: {
+        loaders: [
+            { test: /\.jsx$/,
+                loader: 'babel',
+                include: path.join(__dirname, 'app') },
+            { test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/ },
+            { test: /\.scss?$/,
+                loader: 'style!css!sass',
+                include: path.join(__dirname, 'style') },
+        ]
+    }
+}
