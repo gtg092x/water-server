41b6308e0290cdd86b84c5a893b0b8a088badb7b
gtg092x <mdrake@mediadrake.com>
2016-01-31 15:45:07 -0800

    lowered reading size

--- server/controllers/list-readings.js
+++ server/controllers/list-readings.js
@@ -3,7 +3,7 @@ import Reading from '../models/device-reading'
 
 let ListReadings = (req,res) => {
 
-    Reading.findAll({order:'"createdAt" DESC', limit: 30}).then((readings)=>{
+    Reading.findAll({order:'"createdAt" DESC', limit: 15}).then((readings)=>{
         res.json({readings:readings});
     }).catch((err)=>{
         res.status(500).json({error:err});
